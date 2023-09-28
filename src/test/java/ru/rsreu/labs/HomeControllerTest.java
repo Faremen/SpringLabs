ad8c62a29946be0d224c681d489af67c997cc3cb
faremen <faremanstem@gmail.com>
2023-09-28 22:23:18 +0300

    first commit

---
+++ src/test/java/ru/rsreu/labs/HomeControllerTest.java
@@ -0,0 +1,28 @@
+package ru.rsreu.labs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void homePageTest() throws Exception {
+        mockMvc.perform(get("/")) /*Выполнение GET-запроса*/
+                .andExpect(status().isOk()) /*Ожидание HTTP-ответа 200*/
+                .andExpect(view().name("home")) /*Ожидание ответа "home"*/
+                .andExpect(content().string(containsString("Кальянчик")));
+                
+    }
+}
---
+++ src/test/java/ru/rsreu/labs/LabsApplicationTests.java
@@ -0,0 +1,11 @@
+package ru.rsreu.labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabsApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
